964f513d9a46e63db0bb2945100336a00f297e5d
Darkmauros <vincent22062@orange.fr>
2019-05-17 20:06:49 +0200

    change case

---
+++ Java/Plants-VS-Zombies/src/enums/EnumReloadTime.java
@@ -0,0 +1,12 @@
+package enums;
+
+public class EnumReloadTime {
+
+	public static float very_fast = 2f;
+	public static float fast = 5f;
+	public static float medium = 10f;
+	public static float slow = 15f;
+	public static float very_slow = 30f;
+
+	
+}
